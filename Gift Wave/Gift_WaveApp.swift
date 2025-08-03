da195d188dc7c6fe66edc83a108a06b0386db9d4
Abdullah Latif <L1f21bsse0139@ucp.edu.pk>
2025-08-04 03:00:28 +0500

    Initial Commit

---
+++ Gift Wave/Gift_WaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gift_WaveApp.swift
+//  Gift Wave
+//
+//  Created by Abdullah Latif on 04/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct Gift_WaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Gift WaveTests/Gift_WaveTests.swift
@@ -0,0 +1,17 @@
+//
+//  Gift_WaveTests.swift
+//  Gift WaveTests
+//
+//  Created by Abdullah Latif on 04/08/2025.
+//
+
+import Testing
+@testable import Gift_Wave
+
+struct Gift_WaveTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
